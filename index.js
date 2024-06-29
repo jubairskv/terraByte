5966e8b769a8cc976b707d943de68a4ff79011ad
jubairskv <jubairskv@gmail.com>
2024-06-29 15:30:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Given an IPV4 address, check if it is valid, and return the respective parts if it is valid, else return 0.0.0.0.
+
+function hostAddress(){
+    for(let i=0;i=hostId.length;i++){
+        if(hostId==="9999.12.148.12"){
+            console.log(hostId,"It is valid")
+        }
+        else{
+            console.log("0.0.0.0")
+        }
+    }
+}
+     
+console.log(hostAddress())
+
+if(typeof x === undefined) {
+    console.log("X IS UNDEFINED");
+    x = 10;
+} else {
+    console.log("X HAS VALUE");
+    x = 20;
+}
+var x = 5;
+
+
+console.log("X=", x);
